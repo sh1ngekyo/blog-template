aee5b979d1932801b9d0a09b16cb850c829fa869
sh1ngekyo <repllimate@gmail.com>
2023-09-09 18:22:57 +0300

    Add IApplicationDbContextInterface for infrastructure

---
+++ BlogTemplate.Application/Abstractions/Database/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogTemplate.Application.Abstractions.Database
+{
+    public interface IApplicationDbContext : IDisposable
+    {
+        public DbSet<Domain.Models.ApplicationUser>? ApplicationUsers { get; set; }
+        public DbSet<Domain.Models.Post>? Posts { get; set; }
+        public DbSet<Domain.Models.Page>? Pages { get; set; }
+        public DbSet<Domain.Models.Setting>? Settings { get; set; }
+        public DbSet<Domain.Models.Comment>? Comments { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
