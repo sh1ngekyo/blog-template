47485fcd5e99b3a733d4b3cb46caee06955c3155
sh1ngekyo <repllimate@gmail.com>
2023-09-09 18:31:35 +0300

    Implement scan assemblies with reflection for AutoMapper

---
+++ BlogTemplate.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using BlogTemplate.Application.Abstractions.Mappings;
+using System.Reflection;
+
+namespace BlogTemplate.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
