9bbf4203a850e9c5b5aaa0737c1f414cfba5efd4
sh1ngekyo <repllimate@gmail.com>
2023-09-09 18:26:13 +0300

    Add proxy interfaces for aspnet user/signin/role managers internals

---
+++ BlogTemplate.Application/Abstractions/IUserManagerProxy.cs
@@ -0,0 +1,19 @@
+﻿namespace BlogTemplate.Application.Abstractions
+{
+    public interface IUserManagerProxy<T> : IDisposable where T : class
+    {
+        IQueryable<T?> Users { get; }
+        Task<T?> FindByEmailAsync(string email);
+        Task<T?> FindByUserNameAsync(string userName);
+        Task<T?> FindByIdAsync(string id);
+        Task<bool> CreateAsync(T user, string password);
+        Task<bool> AddToRoleAsync(T user, string role);
+        Task<bool> RemoveFromRoleAsync(T user, string role);
+        Task<IList<string>> GetRolesAsync(T user);
+        Task<string> GeneratePasswordResetTokenAsync(T user);
+        Task<bool> ResetPasswordAsync(T user, string token, string newPassword);
+        Task<bool> CheckPasswordAsync(T user, string password);
+        Task<bool> UpdateAsync(T user);
+        Task<bool> DeleteAsync(T user);
+    }
+}
