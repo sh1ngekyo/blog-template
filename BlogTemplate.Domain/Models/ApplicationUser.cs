069b0e7905810e86ad38b5d16e2cc6573556214b
sh1ngekyo <repllimate@gmail.com>
2023-09-09 18:16:48 +0300

    Add domain models and necessary packages

---
+++ BlogTemplate.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlogTemplate.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? About { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ BlogTemplate.Domain/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Domain.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int? ParentId { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateModified { get; set; }
+        public Comment? Parent { get; set; }
+        public ICollection<Comment>? Children { get; set; }
+    }
+}
