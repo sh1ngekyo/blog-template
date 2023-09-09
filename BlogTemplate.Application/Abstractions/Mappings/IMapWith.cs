09c1b36ad1abdf0e52f9fb3e8c66457825ae6bd3
sh1ngekyo <repllimate@gmail.com>
2023-09-09 18:24:29 +0300

    Add Automapper

---
+++ BlogTemplate.Application/Abstractions/Mappings/IMapWith.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace BlogTemplate.Application.Abstractions.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
