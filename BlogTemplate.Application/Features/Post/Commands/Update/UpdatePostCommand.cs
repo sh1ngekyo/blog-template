da32473bf2bcaf5bf8431f97abfece5a475e14d2
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:17:00 +0300

    Implement update post command

---
+++ BlogTemplate.Application/Features/Post/Commands/Update/UpdatePostCommand.cs
@@ -0,0 +1,21 @@
+﻿using BlogTemplate.Application.Abstractions;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Post.Commands.Update
+{
+    public class UpdatePostCommand : IRequest<Result<UpdatePostCommandResponse>>
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public string? ThumbnailUrl { get; set; }
+    }
+}
---
+++ BlogTemplate.Application/Features/Post/Commands/Update/UpdatePostCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BlogTemplate.Application.Features.Post.Commands.Update
+{
+    public class UpdatePostCommandValidator : AbstractValidator<UpdatePostCommand>
+    {
+        public UpdatePostCommandValidator()
+        {
+            RuleFor(x => x.Title).NotNull().NotEmpty();
+        }
+    }
+}
