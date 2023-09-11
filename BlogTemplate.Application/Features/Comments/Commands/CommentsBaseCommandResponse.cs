7997f4d5e79991db90fd20ed6498a351b980f938
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:49:47 +0300

    Implement Create comment command

---
+++ BlogTemplate.Application/Features/Comments/Commands/CommentsBaseCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Comments.Commands
+{
+    public class CommentsBaseCommandResponse
+    {
+        public string PostSlug { get; set; }
+    }
+}
