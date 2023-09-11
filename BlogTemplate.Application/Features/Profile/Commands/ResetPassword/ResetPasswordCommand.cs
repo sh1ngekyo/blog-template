342588dfa3f7edc11c784e02b012792c4391c473
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:35:41 +0300

    Add tests for ResetPassword command

--- BlogTemplate.Application/Features/Profile/Commands/ResetPassword/ResetPasswordCommand.cs
+++ BlogTemplate.Application/Features/Profile/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -2,13 +2,6 @@
 
 using MediatR;
 
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BlogTemplate.Application.Features.Profile.Commands.ResetPassword
 {
     public class ResetPasswordCommand : IRequest<Result>
--- BlogTemplate.Application/Features/Profile/Commands/ResetPassword/ResetPasswordCommandHandler.cs
+++ BlogTemplate.Application/Features/Profile/Commands/ResetPassword/ResetPasswordCommandHandler.cs
@@ -1,17 +1,6 @@
-﻿using BlogTemplate.Application.Abstractions.Database;
-using BlogTemplate.Application.Abstractions.Enums;
+﻿using BlogTemplate.Application.Abstractions.Enums;
 using BlogTemplate.Application.Abstractions;
-using BlogTemplate.Application.Features.Post.Commands.Delete;
 using BlogTemplate.Domain.Models;
-using BlogTemplate.Domain;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MediatR;
 
 namespace BlogTemplate.Application.Features.Profile.Commands.ResetPassword
