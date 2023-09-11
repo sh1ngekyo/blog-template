b10ece6d009b2b40157e3a4395b3276a3aa515b2
sh1ngekyo <repllimate@gmail.com>
2023-09-11 23:23:58 +0300

    Implement Home page with pagination

---
+++ BlogTemplate.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using BlogTemplate.Application.Features.Home.Queries.GetByPageNumber;
+using BlogTemplate.Domain.Models;
+using BlogTemplate.Presentation.Abstractions.Controller;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BlogTemplate.Presentation.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public async Task<IActionResult> Index(int? page)
+        {
+            var response = await Mediator.Send(new GetHomeByPageNumberQuery()
+            {
+                Page = page ?? 1,
+                PageSize = 4
+            });
+            return View(response.Output);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- BlogTemplate.Presentation/wwwroot/blog/js/scripts.js
+++ BlogTemplate.Presentation/wwwroot/blog/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.8 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
