817c5d6dac572e3d67da17255a110da57aa44359
sh1ngekyo <repllimate@gmail.com>
2023-09-11 16:32:45 +0300

    Implement GetAllSettings query

---
+++ BlogTemplate.Application/Features/Settings/Queries/GetAll/GetAllSettingsQuery.cs
@@ -0,0 +1,17 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.Settings;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Settings.Queries.GetAll
+{
+    public class GetAllSettingsQuery : IRequest<Result<List<SettingsDto>>>
+    {
+    }
+}
---
+++ BlogTemplate.Application/Features/Settings/Queries/GetAll/GetAllSettingsQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BlogTemplate.Application.Abstractions.Database;
+using BlogTemplate.Application.Abstractions.Enums;
+using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.Home;
+using BlogTemplate.Application.Features.Home.Queries.GetByPageNumber;
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogTemplate.Application.DataTransfer.Settings;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper.QueryableExtensions;
+
+namespace BlogTemplate.Application.Features.Settings.Queries.GetAll
+{
+    public class GetAllSettingsQueryHandler : IRequestHandler<GetAllSettingsQuery, Result<List<SettingsDto>>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public GetAllSettingsQueryHandler(IApplicationDbContext context, IMapper mapper) =>
+            (_context, _mapper) = (context, mapper);
+
+        public async Task<Result<List<SettingsDto>>> Handle(GetAllSettingsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var settings = await _context.Settings!.ProjectTo<SettingsDto>(_mapper.ConfigurationProvider)!.ToListAsync();
+            return new Result<List<SettingsDto>>(ResultType.Ok).SetOutput(settings);
+        }
+    }
+}
