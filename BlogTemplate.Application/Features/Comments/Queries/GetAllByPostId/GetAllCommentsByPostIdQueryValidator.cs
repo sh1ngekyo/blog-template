59051b971c8bfc11bcfab9aaf84a6cf4b4896428
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:58:30 +0300

    Implement get all comments query by post id

---
+++ BlogTemplate.Application/Features/Comments/Queries/GetAllByPostId/GetAllCommentsByPostIdQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BlogTemplate.Application.Features.Comments.Queries.GetAllByPostId
+{
+    public class GetAllCommentsByPostIdQueryValidator : AbstractValidator<GetAllCommentsByPostIdQuery>
+    {
+        public GetAllCommentsByPostIdQueryValidator()
+        {
+            RuleFor(x => x.PostId).NotNull().NotEmpty();
+        }
+    }
+}
