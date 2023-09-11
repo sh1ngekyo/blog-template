78b5c3de700c0dfa05b243b0756c4ff9dd7c25e9
sh1ngekyo <repllimate@gmail.com>
2023-09-11 16:40:12 +0300

    Add tests for get all settings query

---
+++ BlogTemplate.Tests/Features/Settings/Queries/GetAllSettingsQueryTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BlogTemplate.Application.Features.Settings.Queries.GetAll;
+using BlogTemplate.Infrastructure.Data;
+
+using Xunit;
+
+namespace BlogTemplate.Tests.Features.Settings.Queries
+{
+    [Collection("SettingsQueryCollection")]
+    public class GetAllSettingsQueryTest
+    {
+        private readonly ApplicationDbContext Context;
+        private readonly IMapper Mapper;
+
+        public GetAllSettingsQueryTest(SettingsQueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task GetAllSettingsQueryHandler_Success()
+        {
+            var handler = new GetAllSettingsQueryHandler(Context, Mapper);
+
+            var response = await handler.Handle(
+                new GetAllSettingsQuery(),
+                CancellationToken.None);
+
+            Assert.True(response.Conclusion);
+            Assert.NotNull(response.Output);
+            Assert.NotEmpty(response.Output);
+        }
+    }
+}
---
+++ BlogTemplate.Tests/Features/Settings/Queries/SettingsQueryTestFixture.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+
+using BlogTemplate.Application.Abstractions.Database;
+using BlogTemplate.Application.Common.Mappings;
+using BlogTemplate.Infrastructure.Data;
+using BlogTemplate.Tests.Common;
+using BlogTemplate.Tests.Common.Extensions.DbContext.Settings;
+
+using Xunit;
+
+namespace BlogTemplate.Tests.Features.Settings.Queries
+{
+    public class SettingsQueryTestFixture : IDisposable
+    {
+        public ApplicationDbContext Context;
+        public IMapper Mapper;
+
+        public SettingsQueryTestFixture()
+        {
+            Context = DbContextBuilder.NewContext.AddSettings().Build();
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AssemblyMappingProfile(
+                    typeof(IApplicationDbContext).Assembly));
+            });
+            Mapper = configurationProvider.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+            Context.Dispose();
+        }
+    }
+
+    [CollectionDefinition("SettingsQueryCollection")]
+    public class SettingsQueryCollection : ICollectionFixture<SettingsQueryTestFixture> { }
+}
