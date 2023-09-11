1c0aeb0873ee27b471584131d35462721581b9ca
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:03:21 +0300

    Implement GetAll users query in feature user

---
+++ BlogTemplate.Application/Features/User/Queries/GetAll/GetAllUsersQuery.cs
@@ -0,0 +1,17 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.User;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.User.Queries.GetAll
+{
+    public class GetAllUsersQuery : IRequest<Result<List<UserDto>>>
+    {
+    }
+}
