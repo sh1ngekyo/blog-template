e15b84823e9796062a63c1a12dace62eda45a88c
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:56:45 +0300

    Add tests for update comment command

--- BlogTemplate.Application/Features/Comments/Commands/CommentsBaseCommandResponse.cs
+++ BlogTemplate.Application/Features/Comments/Commands/CommentsBaseCommandResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlogTemplate.Application.Features.Comments.Commands
+﻿namespace BlogTemplate.Application.Features.Comments.Commands
 {
     public class CommentsBaseCommandResponse
     {
--- BlogTemplate.Application/Features/Comments/Commands/Update/UpdateCommentCommand.cs
+++ BlogTemplate.Application/Features/Comments/Commands/Update/UpdateCommentCommand.cs
@@ -1,12 +1,5 @@
 ﻿using BlogTemplate.Application.Abstractions;
-using BlogTemplate.Domain.Models;
 using MediatR;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlogTemplate.Application.Features.Comments.Commands.Update
 {
