cfa29e084c2d3d3b038acf09e251e2541debc57d
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:59:46 +0300

    Add tests for GetAllCommentsByPostId query

---
+++ BlogTemplate.Tests/Features/Comments/Queries/CommentsTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using BlogTemplate.Infrastructure.Data;
+using BlogTemplate.Tests.Common;
+
+using Xunit;
+using BlogTemplate.Tests.Common.Extensions.DbContext.Comments;
+
+namespace BlogTemplate.Tests.Features.Comments.Queries
+{
+    public class CommentQueryTestFixture
+    {
+        public ApplicationDbContext Context;
+
+        public CommentQueryTestFixture()
+        {
+            Context = DbContextBuilder.NewContext.AddComments().Build();
+        }
+    }
+
+    [CollectionDefinition("CommentQueryCollection")]
+    public class CommentQueryCollection : ICollectionFixture<CommentQueryTestFixture> { }
+}
---
+++ BlogTemplate.Tests/Features/Comments/Queries/GetAllCommentsByPostIdQueryTest.cs
@@ -0,0 +1,41 @@
+﻿using BlogTemplate.Application.Features.Comments.Queries.GetAllByPostId;
+using BlogTemplate.Infrastructure.Data;
+
+using Xunit;
+
+namespace BlogTemplate.Tests.Features.Comments.Queries
+{
+    [Collection("CommentQueryCollection")]
+    public class GetAllCommentsByPostIdQueryTest
+    {
+        private readonly ApplicationDbContext Context;
+        public GetAllCommentsByPostIdQueryTest(CommentQueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+        }
+
+        [Fact]
+        public async Task GetAllCommentsByPostIdQueryHandler_Success()
+        {
+            var handler = new GetAllCommentsByPostIdQueryHandler(Context);
+            var response = await handler.Handle(
+                new GetAllCommentsByPostIdQuery()
+                {
+                    PostId = 1
+                },
+                CancellationToken.None);
+
+            Assert.True(response.Conclusion);
+            Assert.NotNull(response.Output);
+            Assert.Equal(
+                Context.Comments.Where(x => x.PostId == 1 && x.ParentId == null).Count(),
+                response.Output.Count);
+            foreach(var comment in response.Output)
+            {
+                Assert.Equal(
+                    Context.Comments.Where(x => x.ParentId == comment.CommentId).Count(),
+                    comment.Children.Count);
+            }
+        }
+    }
+}
