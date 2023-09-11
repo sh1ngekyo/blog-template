e9fd24cd02c426cdb3a9a6dfa688c209030e32ca
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:37:31 +0300

    Implement Update command for profile feature

---
+++ BlogTemplate.Application/Features/Profile/Commands/Update/UpdateProfileCommand.cs
@@ -0,0 +1,23 @@
+﻿using BlogTemplate.Application.Abstractions;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Profile.Commands.Update
+{
+    public class UpdateProfileCommand : IRequest<Result<UpdateProfileCommandResponse>>
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? About { get; set; }
+        public string? ThumbnailUrl { get; set; }
+    }
+}
