ce2feeac2c2a30e8c5c63c8f4e1753ed18e8c566
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:11:06 +0300

    Implement create post command

---
+++ BlogTemplate.Application/DataTransfer/Post/PostDto.cs
@@ -0,0 +1,41 @@
+﻿using BlogTemplate.Application.Abstractions.Mappings;
+using BlogTemplate.Domain.Models;
+
+namespace BlogTemplate.Application.DataTransfer.Post
+{
+    public class PostDto : IMapWith<Domain.Models.Page>
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? AuthorName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Comment>? Comments { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+
+        public void Mapping(AutoMapper.Profile profile)
+        {
+            profile.CreateMap<Domain.Models.Post, PostDto>()
+                .ForMember(postDto => postDto.Id,
+                    opt => opt.MapFrom(post => post.Id))
+                .ForMember(postDto => postDto.Title,
+                    opt => opt.MapFrom(post => post.Title))
+                .ForMember(postDto => postDto.AuthorName,
+                    opt => opt.MapFrom(post => post.ApplicationUser!.UserName))
+                .ForMember(postDto => postDto.CreatedDate,
+                    opt => opt.MapFrom(post => post.CreatedDate))
+                .ForMember(postDto => postDto.Comments,
+                    opt => opt.MapFrom(post => post.Comments))
+                .ForMember(postDto => postDto.ThumbnailUrl,
+                    opt => opt.MapFrom(post => post.ThumbnailUrl))
+                .ForMember(postDto => postDto.ShortDescription,
+                    opt => opt.MapFrom(post => post.ShortDescription))
+                .ForMember(postDto => postDto.Slug,
+                    opt => opt.MapFrom(post => post.Slug))
+                .ForMember(postDto => postDto.Description,
+                    opt => opt.MapFrom(post => post.Description));
+        }
+    }
+}
---
+++ BlogTemplate.Application/Features/Post/Commands/Create/CreatePostCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BlogTemplate.Application.Abstractions.Enums;
+using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.Features.User.Commands.ChangeRole;
+using BlogTemplate.Domain.Models;
+using BlogTemplate.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogTemplate.Application.Abstractions.Database;
+
+namespace BlogTemplate.Application.Features.Post.Commands.Create
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, Result>
+    {
+        private readonly IUserManagerProxy<ApplicationUser> _userManager;
+        private readonly IApplicationDbContext _context;
+        public CreatePostCommandHandler(IUserManagerProxy<ApplicationUser> userManager, IApplicationDbContext context) =>
+            (_userManager, _context) = (userManager, context);
+
+        public async Task<Result> Handle(CreatePostCommand request,
+            CancellationToken cancellationToken)
+        {
+            var loggedInUser = await _userManager.FindByUserNameAsync(request.UserName!);
+
+            var post = new Domain.Models.Post();
+
+            post.Title = request.Title;
+            post.Description = request.Description;
+            post.ShortDescription = request.ShortDescription;
+            post.ApplicationUserId = loggedInUser!.Id;
+            post.ThumbnailUrl = request.ThumbnailUrl;
+
+            if (post.Title != null)
+            {
+                string slug = request.Title!.Trim();
+                slug = slug.Replace(" ", "-");
+                post.Slug = slug + "-" + Guid.NewGuid();
+            } 
+
+            await _context.Posts!.AddAsync(post);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new Result(ResultType.Created);
+        }
+    }
+}
