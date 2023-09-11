90433ca0e0c7a2a9720f1957db08abf7a899d342
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:43:24 +0300

    Implement GetMyProfileByUserName query in profile feature

---
+++ BlogTemplate.Application/Features/Profile/Queries/GetMyProfileByName/GetMyProfileByNameQuery.cs
@@ -0,0 +1,12 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.User;
+
+using MediatR;
+
+namespace BlogTemplate.Application.Features.Profile.Queries.GetMyProfileByName
+{
+    public class GetMyProfileByNameQuery : IRequest<Result<UserDto>>
+    {
+        public string? UserName { get; set; }
+    }
+}
