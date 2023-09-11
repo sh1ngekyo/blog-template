a2f0c3107f3e57aa28c2b6673dcbbd9024e2377c
sh1ngekyo <repllimate@gmail.com>
2023-09-11 17:40:23 +0300

    Add tests for change role command in feature user

---
+++ BlogTemplate.Tests/Common/TestUserManagerCommandBase.cs
@@ -0,0 +1,15 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Domain.Models;
+
+namespace BlogTemplate.Tests.Common
+{
+    public class TestUserManagerCommandBase
+    {
+        protected readonly IUserManagerProxy<ApplicationUser> UserManager;
+
+        public TestUserManagerCommandBase(IUserManagerProxy<ApplicationUser> userManager)
+        {
+            UserManager = userManager;
+        }
+    }
+}
