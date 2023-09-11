552ba30481fff9e9e2c077581e98be4935c393fe
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:24:14 +0300

    Implement GetPostBySlug query

---
+++ BlogTemplate.Application/Features/Post/Queries/GetBySlug/GetPostBySlugQuery.cs
@@ -0,0 +1,18 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.Post;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Post.Queries.GetBySlug
+{
+    public class GetPostBySlugQuery : IRequest<Result<PostDto>>
+    {
+        public string? Slug { get; set; }
+    }
+}
---
+++ BlogTemplate.Application/Features/Post/Queries/GetBySlug/GetPostBySlugValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace BlogTemplate.Application.Features.Post.Queries.GetBySlug
+{
+    public class GetPostBySlugValidator : AbstractValidator<GetPostBySlugQuery>
+    {
+        public GetPostBySlugValidator()
+        {
+            RuleFor(x => x.Slug).NotNull().NotEmpty();
+        }
+    }
+}
