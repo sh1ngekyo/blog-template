4953f760ff8b18f0ee13f2f0fbe006da0c9871be
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:19:25 +0300

    Implement GetAllPostsForCurrentUser query

---
+++ BlogTemplate.Application/Features/Post/Queries/GetAllForCurrentUser/GetAllPostsForCurrentUserQuery.cs
@@ -0,0 +1,20 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.Post;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using X.PagedList;
+
+namespace BlogTemplate.Application.Features.Post.Queries.GetAllForCurrentUser
+{
+    public class GetAllPostsForCurrentUserQuery : IRequest<Result<List<PostDto>>>
+    {
+        public string? UserName { get; set; }
+    }
+}
