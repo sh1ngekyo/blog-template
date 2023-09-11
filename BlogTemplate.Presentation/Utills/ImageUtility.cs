21b268d1b7912751e483dd6c0eab87d1a2d135e0
sh1ngekyo <repllimate@gmail.com>
2023-09-11 23:10:24 +0300

    Add ImageUtility for save/remove images on the server

---
+++ BlogTemplate.Presentation/Utills/ImageUtility.cs
@@ -0,0 +1,33 @@
+﻿namespace BlogTemplate.Presentation.Utills
+{
+    public class ImageUtility
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageUtility(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public string Upload(IFormFile file)
+        {
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            var uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+            var filePath = Path.Combine(folderPath, uniqueFileName);
+            using (FileStream fileStream = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fileStream);
+            }
+            return uniqueFileName;
+        }
+
+        public void Remove(string? thumbnailUrl)
+        {
+            if (string.IsNullOrWhiteSpace(thumbnailUrl))
+                return;
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "thumbnails");
+            var filePath = Path.Combine(folderPath, thumbnailUrl);
+            System.IO.File.Delete(filePath);
+        }
+    }
+}
