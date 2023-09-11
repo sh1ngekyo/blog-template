8a1a2b6096c67a2b7f0212f63a957be5db103107
sh1ngekyo <repllimate@gmail.com>
2023-09-11 17:07:19 +0300

    Implement & test GetBySlug query for feature page

---
+++ BlogTemplate.Application/Features/Page/Queries/GetBySlug/GetPageBySlugQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.Abstractions.Database;
+using BlogTemplate.Application.Abstractions.Enums;
+using BlogTemplate.Application.DataTransfer.Page;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogTemplate.Application.Features.Page.Queries.GetBySlug
+{
+    public class GetPageBySlugQueryHandler 
+        : IRequestHandler<GetPageBySlugQuery, Result<PageDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public GetPageBySlugQueryHandler(IApplicationDbContext context, IMapper mapper) =>
+            (_context, _mapper) = (context, mapper);
+
+        public async Task<Result<PageDto>> Handle(GetPageBySlugQuery request,
+            CancellationToken cancellationToken)
+        {
+            var page = await _context.Pages!.FirstOrDefaultAsync(x => x.Slug == request.Slug);
+            return page is not null ? 
+                new Result<PageDto>(ResultType.Ok).SetOutput(_mapper.Map<PageDto>(page)) 
+                : new Result<PageDto>(ErrorType.NotFound);
+        }
+    }
+}
---
+++ BlogTemplate.Tests/Features/Page/Queries/GetPageBySlugQueryTest.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using BlogTemplate.Application.Abstractions.Enums;
+using BlogTemplate.Infrastructure.Data;
+using Xunit;
+using BlogTemplate.Application.Features.Page.Queries.GetBySlug;
+using BlogTemplate.Tests.Common.Extensions.DbContext.Page;
+
+namespace BlogTemplate.Tests.Features.Page.Queries
+{
+    [Collection("PageQueryCollection")]
+    public class GetPageBySlugQueryTest
+    {
+
+        private readonly ApplicationDbContext Context;
+        private readonly IMapper Mapper;
+
+        public GetPageBySlugQueryTest(PageQueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task GetPageBySlugQueryHandler_Success()
+        {
+            var handler = new GetPageBySlugQueryHandler(Context, Mapper);
+
+            var response = await handler.Handle(
+                new GetPageBySlugQuery()
+                {
+                    Slug = DbContextAddPageExtension.PageSlug
+                },
+                CancellationToken.None);
+
+            Assert.True(response.Conclusion);
+            Assert.NotNull(response.Output);
+            Assert.NotNull(response.Output.Title);
+            Assert.Equal(DbContextAddPageExtension.PageId, response.Output.Id);
+        }
+
+        [Fact]
+        public async Task GetPageBySlugQueryHandler_FailOnWrongSlug()
+        {
+            var handler = new GetPageBySlugQueryHandler(Context, Mapper);
+
+            var response = await handler.Handle(
+                new GetPageBySlugQuery()
+                {
+                    Slug = Guid.NewGuid().ToString()
+                },
+                CancellationToken.None);
+
+            Assert.False(response.Conclusion);
+            Assert.Null(response.Output);
+            Assert.Equal(ErrorType.NotFound, response.ErrorDescription.ErrorType);
+        }
+    }
+}
