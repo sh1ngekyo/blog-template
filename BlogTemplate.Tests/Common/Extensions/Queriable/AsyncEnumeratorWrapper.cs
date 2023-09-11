0e6228110c7c0cd79eb712d76ccd6771fcd7ed82
sh1ngekyo <repllimate@gmail.com>
2023-09-11 17:37:47 +0300

    Implement IQueryProvider & ICollection extension AsAsyncQueryable for test purposes

---
+++ BlogTemplate.Tests/Common/Extensions/Queriable/AsyncEnumeratorWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Tests.Common.Extensions.Queriable
+{
+    internal class AsyncEnumeratorWrapper<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> Source;
+
+        public AsyncEnumeratorWrapper(IEnumerator<T> source)
+        {
+            Source = source;
+        }
+
+        public T Current => Source.Current;
+
+        public ValueTask DisposeAsync()
+        {
+            return new ValueTask(Task.CompletedTask);
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(Source.MoveNext());
+        }
+    }
+}
