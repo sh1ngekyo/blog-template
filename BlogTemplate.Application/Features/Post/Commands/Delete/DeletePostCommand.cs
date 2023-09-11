a5ff48a7144cca19914b5910b14220914f3d2094
sh1ngekyo <repllimate@gmail.com>
2023-09-11 22:14:28 +0300

    Implement delete post command

---
+++ BlogTemplate.Application/Features/Post/Commands/Delete/DeletePostCommand.cs
@@ -0,0 +1,20 @@
+﻿using BlogTemplate.Application.Abstractions;
+using BlogTemplate.Application.DataTransfer.User;
+using BlogTemplate.Application.Features.Comments.Commands.Delete;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTemplate.Application.Features.Post.Commands.Delete
+{
+    public class DeletePostCommand : IRequest<Result<DeletePostCommandResponse>>
+    {
+        public int PostId { get; set; }
+        public string? DeletedByUserName { get; set; }
+    }
+}
